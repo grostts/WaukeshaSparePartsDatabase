a3e4cf9308fef064be15a5d094007bc53689ce29
grostts <73026090+grostts@users.noreply.github.com>
2022-12-24 18:32:13 +0300

    Third commit. Added script for downloading BOMs.

--- WaukeshaMain.py
+++ WaukeshaMain.py
@@ -5,6 +5,8 @@ from AuthorizationData import epic_password
 from AuthorizationData import epic_login
 import pandas as pd
 import glob
+from selenium.webdriver.common.keys import Keys
+
 
 start_time = time.time()
 
@@ -36,24 +38,69 @@ try:
     email_input = driver.find_element(By.ID,'TextBox2')
     email_input.clear()
     email_input.send_keys(epic_login)
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(30)
 
     # entering a password
     password_input = driver.find_element(By.ID,'TextBox3')
     password_input.clear()
     password_input.send_keys(epic_password)
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(30)
 
     # press login button
     login_button_click = driver.find_element(By.ID, 'Button1').click()
-    driver.implicitly_wait(20)
-    time.sleep(10)
+    driver.implicitly_wait(30)
 
+    print('Start downloading the bom files...')
+    counter = 0
+    not_downloaded_list = []
+    for elem in serial_numbers:
+        try:
+            # fill in the search field with the serial number
+            search_field = driver.find_element(By.ID, '40')
+            search_field.clear()
+            search_field.send_keys(elem)
+            search_field.send_keys((Keys.ENTER))
+            driver.implicitly_wait(20)
 
+            # press plus button
+            plus_button = driver.find_element(By.ID, '30')
+            plus_button.click()
+            driver.implicitly_wait(20)
 
+            # press  BOM button
+            BOM_button = driver.find_element(By.ID, '78')
+            BOM_button.click()
+            driver.implicitly_wait(10)
+
+            # switch window and press export to excel button
+            driver.switch_to.window(driver.window_handles[1])
+            excel_button = driver.find_element(By.ID, 'btnExportExcel')
+            excel_button.click()
+            driver.implicitly_wait(20)
+
+            counter += 1
+            current_time = time.time()
+
+            print(f'{counter}/ {total_sn} was downloaded ({elem}). {round(current_time - start_time, 1)} seconds have passed.')
+            driver.close()
+            driver.switch_to.window(driver.window_handles[1])
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
+        except:
+            counter += 1
+            current_time = time.time()
+            print(f'{counter}/ {total_sn} was not downloaded ({elem}). {round(current_time - start_time, 1)} seconds have passed.')
+            not_downloaded_list.append(elem)
+        finally:
+            continue
 
 except Exception as ex:
     print(ex)
 finally:
     driver.close()
     driver.quit()
+print('File downloading completed!')
+
+print('These files were not downloaded:')
+for el in not_downloaded_list:
+    print(el)
